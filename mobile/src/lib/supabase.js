c5be09e1ff08934896d8dc974dd8703cfc0b99f5
Nicolas Lemos <nicolasweirichdelemos@gmail.com>
2025-09-29 00:12:13 -0300

    feat(mobile): add supabase client and new deps

---
+++ mobile/src/lib/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+// As variáveis vêm do .env
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
